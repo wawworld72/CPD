2fedf25de87254dd2b0d6a368b94cb8b94b31c12
wawworld72 <41935668+wawworld72@users.noreply.github.com>
2024-08-10 20:59:49 +0900

    Create __init__.py

---
+++ hello/__init__.py
@@ -0,0 +1,12 @@
+import check50
+import check50.c
+
+@check50.check()
+def compiles():
+    """hello.c compiles"""
+    check50.c.compile("hello.c")
+
+@check50.check(compiles)
+def prints_hello():
+    """prints "hello, world\\n" """
+    check50.run("./hello").stdout("[Hh]ello, world!?\n", regex=True).exit(0)
