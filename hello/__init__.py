db2572e7742ccb7b4c0305a3b2908eb2317d4485
wawworld72 <41935668+wawworld72@users.noreply.github.com>
2024-08-10 21:05:55 +0900

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -2,6 +2,11 @@ import check50
 import check50.c
 
 @check50.check()
+def exists():
+    """hello.c exists"""
+    check50.exists("hello.c")
+    
+@check50.check()
 def compiles():
     """hello.c compiles"""
     check50.c.compile("hello.c")
