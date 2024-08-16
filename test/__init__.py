978e5b381342315e348a3282535b23fb41c234ba
tera5574 <20201403@game.hoseo.edu>
2024-08-16 16:46:59 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -6,17 +6,17 @@ def exists():
     """test.c exists"""
     check50.exists("test.c")
     
-@check50.check()
+@check50.check(exists)
 def compiles():
     """test.c compiles"""
     check50.c.compile("test.c")
 
 @check50.check(compiles)
 def test_input_15():
-    """input of 15 products correct output"""
+    """input of 15 produces correct output"""
     check50.run("./test").stdin("15").stdout("1 2 4 5 7 8 10 \n").exit(0)
 
 @check50.check(compiles)
 def test_input_9():
-    """input of 15 products correct output"""
+    """input of 9 produces correct output"""
     check50.run("./test").stdin("9").stdout("1 2 4 5 7 8 \n").exit(0)
