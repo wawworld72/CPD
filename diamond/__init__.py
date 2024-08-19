0808f64260e303aaf47728418ff608e34623b3ff
tera5574 <20201403@game.hoseo.edu>
2024-08-19 20:40:37 +0900

    Update __init__.py

--- diamond/__init__.py
+++ diamond/__init__.py
@@ -3,20 +3,23 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """diamond.c exists"""
+    check50.exists("diamond.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """diamond.c compiles"""
+    check50.c.compile("diamond.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
-
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+def test_case():
+    """....A....\n
+    ...B.B...\n
+    ..C...C..\n
+    .D.....D.\n
+    E.......E\n
+    .D.....D.\n
+    ..C...C..\n
+    ...B.B...\n
+    ....A...."""
+    check50.run("./diamond").stdout("....A....\n").stdout("...B.B...\n").stdout("..C...C..\n").stdout(".D.....D.\n").stdout("E.......E\n").stdout(".D.....D.\n").stdout("..C...C..\n").stdout("...B.B...\n").stdout("....A....").exit(0)
