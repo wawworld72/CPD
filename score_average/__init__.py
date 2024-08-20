790debcbea86f4a8854b2a3397db55269f7831b9
tera5574 <20201403@game.hoseo.edu>
2024-08-20 20:38:01 +0900

    Update __init__.py

--- score_average/__init__.py
+++ score_average/__init__.py
@@ -16,3 +16,7 @@ def test_input_1():
     """입력 : 1 2 3 4 5 / 기대 출력 : 3.00"""
     check50.run("./score_average").stdin("1\n2\n3\n4\n5").stdout("3.00").exit(0)
 
+@check50.check(compiles)
+def test_input_2():
+    """입력 : 1 3 5 7 9 / 기대 출력 : 5.00"""
+    check50.run("./score_average").stdin("1\n3\n5\n7\n9").stdout("5.00").exit(0)
