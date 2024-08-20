c22093d06eef881f5c65c7c5f96c3fe10ec6313e
tera5574 <20201403@game.hoseo.edu>
2024-08-20 16:16:09 +0900

    Update __init__.py

--- retangles/__init__.py
+++ retangles/__init__.py
@@ -15,3 +15,19 @@ def compiles():
 def test_input_1():
     """\n  입력 x: 1 1 10 10 \n  입력 y: 5 5 10 10 \n  기대 출력 : 사각형들이 충돌합니다."""
     check50.run("./retangles").stdin("1").stdin("1").stdin("10").stdin("10").stdin("5").stdin("5").stdin("10").stdin("10").stdout("사각형들이 충돌합니다.").exit(0)
+
+@check50.check(compiles)
+def test_input_2():
+    """\n  입력 x: 5 5 10 10 \n  입력 y: 1 1 10 10 \n  기대 출력 : 사각형들이 충돌합니다."""
+    check50.run("./retangles").stdin("5").stdin("5").stdin("10").stdin("10").stdin("1").stdin("1").stdin("10").stdin("10").stdout("사각형들이 충돌합니다.").exit(0)
+
+@check50.check(compiles)
+def test_input_3():
+    """\n  입력 x: -1 1 10 10 \n  입력 y: \n  기대 출력 : 잘못된 입력"""
+    check50.run("./retangles").stdin("-1").stdin("0").stdin("10").stdin("10").stdin("").stdin("").stdin("").stdin("").stdout("잘못된 입력").exit(0)
+
+@check50.check(compiles)
+def test_input_4():
+    """\n  입력 x: 1 1 10 10 \n  입력 y: 5 5 10 10 \n  기대 출력 : 사각형들이 충돌합니다."""
+    check50.run("./retangles").stdin("1").stdin("1").stdin("10").stdin("10").stdin("5").stdin("5").stdin("10").stdin("10").stdout("사각형들이 충돌합니다.").exit(0)
+
