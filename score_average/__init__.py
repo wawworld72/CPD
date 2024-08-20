105a24660ed429a0249eb01e36ac9242840a961b
tera5574 <20201403@game.hoseo.edu>
2024-08-20 20:34:55 +0900

    Update __init__.py

--- score_average/__init__.py
+++ score_average/__init__.py
@@ -3,20 +3,16 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """score_average.c exists"""
+    check50.exists("score_average.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """score_average.c compiles"""
+    check50.c.compile("score_average.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
+def test_input_1():
+    """입력 : 1 2 3 4 5 / 기대 출력 : 3.00"""
+    check50.run("./score_average").stdin("1\n2\n3\n4\n5").stdout("3.00").exit(0)
 
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
