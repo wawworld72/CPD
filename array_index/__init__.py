41dff58b07f197d8258c296581f6c3708f068a09
tera5574 <20201403@game.hoseo.edu>
2024-08-20 19:00:05 +0900

    Update __init__.py

--- array_index/__init__.py
+++ array_index/__init__.py
@@ -3,15 +3,15 @@ import check50.c
 
 @check50.check()
 def exists():
-    """array_index.c exists"""
-    check50.exists("array_index.c")
+    """arrayIndex.c exists"""
+    check50.exists("arrayIndex.c")
     
 @check50.check(exists)
 def compiles():
     """array_index.c compiles"""
-    check50.c.compile("array_index.c", lcs50 = True)
+    check50.c.compile("arrayIndex.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_1():
     """입력 : 3 / 기대 출력 : arr[3] = 40"""
-    check50.run("./array_index").stdin("3").stdout("arr[3] = 40\n").exit(0)
+    check50.run("./arrayIndex").stdin("3").stdout("arr[3] = 40\n").exit(0)
