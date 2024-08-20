2e7840bed342a2b27bfa742c88b374128c468069
tera5574 <20201403@game.hoseo.edu>
2024-08-20 21:33:36 +0900

    Update __init__.py

--- common_elements/__init__.py
+++ common_elements/__init__.py
@@ -3,20 +3,15 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """common_elements.c exists"""
+    check50.exists("common_elements.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """common_elements.c compiles"""
+    check50.c.compile("common_elements.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
-
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+    """입력 : 1 2 3 4 5 6 7 8 9 / 기대 출력 : 5"""
+    check50.run("./common_elements").stdin("1\n2\n3\n4\n5\n6\n7\n8\n9").stdout("5").exit(0)
