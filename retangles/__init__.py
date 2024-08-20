a85d7038012568541ac195d520d3f71966acf86a
tera5574 <20201403@game.hoseo.edu>
2024-08-20 16:17:29 +0900

    Update __init__.py

--- retangles/__init__.py
+++ retangles/__init__.py
@@ -23,11 +23,12 @@ def test_input_2():
 
 @check50.check(compiles)
 def test_input_3():
-    """\n  입력 x: -1 1 10 10 \n  입력 y: \n  기대 출력 : 잘못된 입력"""
-    check50.run("./retangles").stdin("-1").stdin("0").stdin("10").stdin("10").stdin("").stdin("").stdin("").stdin("").stdout("잘못된 입력").exit(0)
+    """\n  입력 x: 1 1 10 10 \n  입력 y: 15 15 10 10 \n  기대 출력 : 사각형들이 충돌하지 않습니다."""
+    check50.run("./retangles").stdin("1").stdin("1").stdin("10").stdin("10").stdin("15").stdin("15").stdin("10").stdin("10").stdout("사각형들이 충돌하지 않습니다.").exit(0)
+
 
 @check50.check(compiles)
 def test_input_4():
-    """\n  입력 x: 1 1 10 10 \n  입력 y: 5 5 10 10 \n  기대 출력 : 사각형들이 충돌합니다."""
-    check50.run("./retangles").stdin("1").stdin("1").stdin("10").stdin("10").stdin("5").stdin("5").stdin("10").stdin("10").stdout("사각형들이 충돌합니다.").exit(0)
+    """\n  입력 x: -1 1 10 10 \n  입력 y: \n  기대 출력 : 잘못된 입력"""
+    check50.run("./retangles").stdin("-1").stdin("0").stdin("10").stdin("10").stdin("").stdin("").stdin("").stdin("").stdout("잘못된 입력").exit(0)
 
