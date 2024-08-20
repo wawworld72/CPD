36acfd6c4651d38930b636e72ab79b6eecd75663
tera5574 <20201403@game.hoseo.edu>
2024-08-20 16:29:42 +0900

    Update __init__.py

--- retangles/__init__.py
+++ retangles/__init__.py
@@ -30,5 +30,5 @@ def test_input_3():
 @check50.check(compiles)
 def test_input_4():
     """\n  입력 x: -1 1 10 10 \n  입력 y: \n  기대 출력 : 잘못된 입력"""
-    check50.run("./retangles").stdin("-1").stdin("1").stdin("10").stdin("10").stdin("\n").stdin("\n").stdin("\n").stdin("\n").stdout("잘못된 입력").exit(0)
+    check50.run("./retangles").stdin("-1\n1\n10\n10\n\n\n\n\n").stdout("잘못된 입력").exit(0)
 
