f19e5f30623803ea0b5246dcb903454f972610c2
tera5574 <20201403@game.hoseo.edu>
2024-08-20 13:59:23 +0900

    Update __init__.py

--- sum/__init__.py
+++ sum/__init__.py
@@ -3,20 +3,20 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """sum.c exists"""
+    check50.exists("sum.c")
     
 @check50.check(exists)
 def compiles():
     """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    check50.c.compile("sum.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
+def test_case_1():
+    """입력 : 5 10 / 기대 출력 : 15"""
+    check50.run("./sum").stdin("5").stdin("10").stdout("15").exit(0)
 
 @check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+def test_case_2():
+    """입력 : 20 30 / 기대 출력 : 50"""
+    check50.run("./sum").stdin("20").stdin("30").stdout("50").exit(0)
