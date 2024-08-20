af358a34e50faa8ff5454396510f2f4b53e67e31
tera5574 <20201403@game.hoseo.edu>
2024-08-20 19:42:48 +0900

    Update __init__.py

--- array_index/__init__.py
+++ array_index/__init__.py
@@ -15,3 +15,13 @@ def compiles():
 def test_input_1():
     """입력 : 3 / 기대 출력 : 40"""
     check50.run("./array_index").stdin("3").stdout("40").exit(0)
+
+@check50.check(compiles)
+def test_input_2():
+    """입력 : 0 / 기대 출력 : 10"""
+    check50.run("./array_index").stdin("0").stdout("10").exit(0)
+
+@check50.check(compiles)
+def test_input_3():
+    """입력 : 5 / 기대 출력 : Invalid index"""
+    check50.run("./array_index").stdin("5").stdout("Invalid index").exit(0)
