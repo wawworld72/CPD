fd11e3f96eeb2134de15168bf999d0cdb3086cfe
tera5574 <20201403@game.hoseo.edu>
2024-08-20 21:39:51 +0900

    Update __init__.py

--- common_elements/__init__.py
+++ common_elements/__init__.py
@@ -12,6 +12,11 @@ def compiles():
     check50.c.compile("common_elements.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
+def test_input_1():
     """입력 : 1 2 3 4 5 6 7 8 9 / 기대 출력 : 5"""
     check50.run("./common_elements").stdin("1\n2\n3\n4\n5\n6\n7\n8\n9").stdout("5").exit(0)
+
+@check50.check(compiles)
+def test_input_2():
+    """입력 : 2 4 6 8 10 12 14 15 20 / 기대 출력 : 4"""
+    check50.run("./common_elements").stdin("2\n4\n6\n8\n10\n12\n14\n15\n20").stdout("4").exit(0)
