e8ff6e0f6478e44a80a7a2b0e0c37f82b9fd166c
tera5574 <20201403@game.hoseo.edu>
2024-08-21 16:38:14 +0900

    Update __init__.py

--- palidrome_check/__init__.py
+++ palidrome_check/__init__.py
@@ -3,20 +3,30 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """palidrome_check.c exists"""
+    check50.exists("palidrome_check.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """palidrome_check.c compiles"""
+    check50.c.compile("palidrome_check.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
+def test_input_1():
+    """입력 : 1991 / 기대 출력 : 1991는 참"""
+    check50.run("./palidrome_check").stdin("1991").stdout("1991는 참").exit(0)
 
 @check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+def test_input_2():
+    """입력 : 12321 / 기대 출력 : 12321는 참"""
+    check50.run("./palidrome_check").stdin("12321").stdout("12321는 참").exit(0)
+
+@check50.check(compiles)
+def test_input_3():
+    """입력 : 1234 / 기대 출력 : 1234는 거짓"""
+    check50.run("./palidrome_check").stdin("1234").stdout("1991는 거짓").exit(0)
+
+@check50.check(compiles)
+def test_input_4():
+    """입력 : -1 / 기대 출력 : 잘못된 입력"""
+    check50.run("./palidrome_check").stdin("-1").stdout("잘못된 입력").exit(0)
