3de820f8fbac203baf8d560969b2967bb5bc54d1
20201403 <20201403@game.hoseo.edu>
2024-08-29 20:52:59 +0900

    Update __init__.py

--- mode_finder/__init__.py
+++ mode_finder/__init__.py
@@ -3,20 +3,15 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """mode_finder.c exists"""
+    check50.exists("mode_finder.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """mode_finder.c compiles"""
+    check50.c.compile("mode_finder.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
-
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+def test_input_1():
+    """입력1 : 6 / 입력2: 1 2 2 3 3 3/ 기대 출력 : 3"""
+    check50.run("./mode_finder").stdin("6\n1\n2\n2\n3\n3\n3\n").stdout("3").exit(0)
