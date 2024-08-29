d4a5a2d1a75f29494a1dec027971ab5e2c28ae7a
20201403 <20201403@game.hoseo.edu>
2024-08-29 20:00:09 +0900

    Update __init__.py

--- magic_square/__init__.py
+++ magic_square/__init__.py
@@ -3,20 +3,16 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """magic_square.c exists"""
+    check50.exists("magic_square.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """magic_square.c compiles"""
+    check50.c.compile("magic_square.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
+    """입력 : \n2 7 6 \n9 5 1 \n4 3 8  \n기대 출력 : 이 배열은 마방진입니다."""
+    check50.run("./magic_square").stdin("2\n7\n6\n9\n5\n1\n4\n3\n8\n").stdout("이 배열은 마방진입니다.").exit(0)
 
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
