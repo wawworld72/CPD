d17a5e7193a3bb56e7f9fc602934822027cd9931
20201403 <20201403@game.hoseo.edu>
2024-09-01 21:30:19 +0900

    Update __init__.py

--- ncursesd/__init__.py
+++ ncursesd/__init__.py
@@ -3,20 +3,10 @@ import check50.c
 
 @check50.check()
 def exists():
-    """test.c exists"""
-    check50.exists("test.c")
+    """gamed.c exists"""
+    check50.exists("gamed.c")
     
 @check50.check(exists)
 def compiles():
-    """test.c compiles"""
-    check50.c.compile("test.c", lcs50 = True)
-
-@check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./test").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
-
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./test").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+    """gamed.c compiles"""
+    check50.c.compile("gamed.c", lcs50 = True)
