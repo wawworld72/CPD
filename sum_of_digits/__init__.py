6814ba3612dee63493472e1b96eb282b64f3c177
20201403 <20201403@game.hoseo.edu>
2024-09-04 20:07:19 +0900

    Update __init__.py

--- sum_of_digits/__init__.py
+++ sum_of_digits/__init__.py
@@ -3,20 +3,15 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """sum_of_digits.c exists"""
+    check50.exists("sum_of_digits.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """sum_of_digits.c compiles"""
+    check50.c.compile("sum_of_digits.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
-
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+def test_input_1():
+    """입력 : 123 456 789 -1 / 기대 출력 : 39"""
+    check50.run("./sum_of_digits").stdin("123\n456\n789\n-1").stdout("39").exit(0)
