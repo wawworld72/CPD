06b753b5d356c635c93b59ba3c5fe92695e1d945
20201403 <20201403@game.hoseo.edu>
2024-09-12 16:25:42 +0900

    Update __init__.py

--- longest_word_finder/__init__.py
+++ longest_word_finder/__init__.py
@@ -3,25 +3,16 @@ import check50.c
 
 @check50.check()
 def exists():
-    """arrayIndex.c exists"""
-    check50.exists("array_index.c")
+    """longest_word_finder.c exists"""
+    check50.exists("longest_word_finder.c")
     
 @check50.check(exists)
 def compiles():
-    """array_index.c compiles"""
-    check50.c.compile("array_index.c", lcs50 = True)
+    """longest_word_finder.c compiles"""
+    check50.c.compile("longest_word_finder.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_1():
-    """입력 : 3 / 기대 출력 : 40"""
-    check50.run("./array_index").stdin("3").stdout("40").exit(0)
+    """입력 : hello world programming / 기대 출력 : programming"""
+    check50.run("./longest_word_finder").stdin("hello world programming").stdout("programming").exit(0)
 
-@check50.check(compiles)
-def test_input_2():
-    """입력 : 0 / 기대 출력 : 10"""
-    check50.run("./array_index").stdin("0").stdout("10").exit(0)
-
-@check50.check(compiles)
-def test_input_3():
-    """입력 : 5 / 기대 출력 : Invalid index"""
-    check50.run("./array_index").stdin("5").stdout("Invalid index").exit(0)
