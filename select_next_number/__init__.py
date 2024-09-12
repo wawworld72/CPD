9b8e3259ea2f26d5f556de46ca21cebde6b934c0
20201403 <20201403@game.hoseo.edu>
2024-09-12 16:14:01 +0900

    Update __init__.py

--- select_next_number/__init__.py
+++ select_next_number/__init__.py
@@ -3,25 +3,16 @@ import check50.c
 
 @check50.check()
 def exists():
-    """arrayIndex.c exists"""
-    check50.exists("array_index.c")
+    """select_next_number.c exists"""
+    check50.exists("select_next_number.c")
     
 @check50.check(exists)
 def compiles():
-    """array_index.c compiles"""
-    check50.c.compile("array_index.c", lcs50 = True)
+    """select_next_number.c compiles"""
+    check50.c.compile("select_next_number.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_1():
-    """입력 : 3 / 기대 출력 : 40"""
-    check50.run("./array_index").stdin("3").stdout("40").exit(0)
+    """입력1 : 1 2 3 4 5 4 3 -1, 입력2 : 1 / 기대 출력 : 2345"""
+    check50.run("./select_next_number").stdin("1\n2\n3\n4\n5\n4\n3\n-1\n1").stdout("2345").exit(0)
 
-@check50.check(compiles)
-def test_input_2():
-    """입력 : 0 / 기대 출력 : 10"""
-    check50.run("./array_index").stdin("0").stdout("10").exit(0)
-
-@check50.check(compiles)
-def test_input_3():
-    """입력 : 5 / 기대 출력 : Invalid index"""
-    check50.run("./array_index").stdin("5").stdout("Invalid index").exit(0)
