0e2d12c38611c0f9ff4b233007f0250ef3b8cacd
20201403 <20201403@game.hoseo.edu>
2024-09-26 15:19:39 +0900

    Update __init__.py

--- longest_word_finder/__init__.py
+++ longest_word_finder/__init__.py
@@ -9,7 +9,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """longest_word_finder.c compiles"""
-    check50.c.compile("longest_word_finder.c", lcs50 = True)
+    check50.c.compile("longest_word_finder.c")
 
 @check50.check(compiles)
 def test_input_1():
