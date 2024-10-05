48c6a9538acdce2cc06ddff0953ac7409ef38dca
20201403 <20201403@game.hoseo.edu>
2024-10-05 19:24:59 +0900

    Update __init__.py

--- longest_word_finder/__init__.py
+++ longest_word_finder/__init__.py
@@ -15,3 +15,13 @@ def compiles():
 def test_input_1():
     """입력 : hello world programming / 기대 출력 : programming"""
     check50.run("./longest_word_finder").stdin("hello world programming\n").stdout("programming\n").exit(0)
+
+@check50.check(compiles)
+def test_input_2():
+    """입력 : icecream makes everything better / 기대 출력 : everything"""
+    check50.run("./longest_word_finder").stdin("icecream makes everything better\n").stdout("everything\n").exit(0)
+
+@check50.check(compiles)
+def test_input_3():
+    """입력 : cat bat sat fat / 기대 출력 : bat"""
+    check50.run("./longest_word_finder").stdin("cat bat sat fat\n").stdout("bat\n").exit(0)
