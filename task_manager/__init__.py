00f93c08d7ac5d016a491c740cc4562a86f49ab2
20201403 <20201403@game.hoseo.edu>
2024-10-06 16:16:21 +0900

    Update __init__.py

--- task_manager/__init__.py
+++ task_manager/__init__.py
@@ -25,8 +25,7 @@ def add_tesk_1():
 
 @check50.check(compiles)
 def add_tesk_2():
-    """일정 관리 프로그램에 오신 것을 환영합니다!\n
-    \n
+    """
     1. 새 일정 추가\n
     2. 일정 목록 보기\n
     3. 프로그램 종료\n
@@ -37,8 +36,7 @@ def add_tesk_2():
 
 @check50.check(compiles)
 def view_tesk():
-    """일정 관리 프로그램에 오신 것을 환영합니다!\n
-    \n
+    """
     1. 새 일정 추가\n
     2. 일정 목록 보기\n
     3. 프로그램 종료\n
@@ -46,4 +44,4 @@ def view_tesk():
     일정 목록:\n
     일정 1: 12시 - 오후\n
     일정 2: 20시 - 저녁"""
-    check50.run("./task_manager").stdin("1\n12\n1\20\n2\n3\n").stdout("일정 1: 12시 - 오후\n일정 2: 20시 - 저녁").exit(0)
+    check50.run("./task_manager").stdin("1\n12\n1\n20\n2\n3\n").stdout("일정 1: 12시 - 오후\n일정 2: 20시 - 저녁").exit(0)
