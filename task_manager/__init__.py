acef7b95ab4fe5ae6d254a3bc8ad9d698a25dfc7
20201403 <20201403@game.hoseo.edu>
2024-10-06 16:08:01 +0900

    Update __init__.py

--- task_manager/__init__.py
+++ task_manager/__init__.py
@@ -12,7 +12,7 @@ def compiles():
     check50.c.compile("task_manager.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_1():
+def add_tesk_1():
     """일정 관리 프로그램에 오신 것을 환영합니다!\n
     \n
     1. 새 일정 추가\n
@@ -22,3 +22,15 @@ def test_input_1():
     일정의 시간을 입력하세요 (0-23): 12\n
     일정이 추가되었습니다."""
     check50.run("./task_manager").stdin("1\n12\n3\n").stdout("일정이 추가되었습니다.\n").exit(0)
+
+@check50.check(compiles)
+def add_tesk_2():
+    """일정 관리 프로그램에 오신 것을 환영합니다!\n
+    \n
+    1. 새 일정 추가\n
+    2. 일정 목록 보기\n
+    3. 프로그램 종료\n
+    원하는 작업을 선택하세요: 1\n
+    일정의 시간을 입력하세요 (0-23): 20\n
+    일정이 추가되었습니다."""
+    check50.run("./task_manager").stdin("1\n20\n3\n").stdout("일정이 추가되었습니다.\n").exit(0)
