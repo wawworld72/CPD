dd33b953a83562bc45fea632fc198d77f53c85c1
20201403 <20201403@game.hoseo.edu>
2024-10-10 14:55:26 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -16,4 +16,5 @@ def test_add_student():
     """학생 추가 + 짝수 성적 출력 테스트"""
     check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n").stdout("학생이 추가되었습니다.")\
     .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
-    .stdin("2\n").stdout("짝수 성적의 개수: 8")
+    .stdin("2\n").stdout("짝수 성적의 개수: 8")\
+    .stdin("5\n").stdout("프로그램을 종료합니다. 안녕히 가세요!")
