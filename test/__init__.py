3b0c79d8e49a8559534bfadbb9260b4238d183c1
20201403 <20201403@game.hoseo.edu>
2024-10-10 14:25:47 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -14,37 +14,5 @@ def compiles():
 @check50.check(compiles)
 def test_add_student():
     """학생 추가 기능이 정상적으로 작동하는지 테스트합니다."""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n5\n")\
-        .stdout("학생이 추가되었습니다.\n", regex=False)\
-        .stdout("원하는 작업을 선택하세요: ", regex=False)\
-        .exit(0)
+    check50.run("./test").stdin("1\n2024\n51\n50\n60\n50\\1").stdout("학생이 추가되었습니다.\n")
 
-@check50.check(compiles)
-def test_even_grades_count():
-    """짝수 성적의 개수를 정확하게 세는지 테스트합니다."""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n")\
-        .stdin("1\n2023\n81\n80\n90\n80\n80\n")\
-        .stdin("2\n2\n")\
-        .stdout("짝수 성적의 개수: 8\n", regex=False)\
-        .stdout("원하는 작업을 선택하세요: ", regex=False)\
-        .exit(0)
-
-@check50.check(compiles)
-def test_average_grade():
-    """학생 평균 성적 계산이 정상적으로 동작하는지 테스트합니다."""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n")\
-        .stdin("1\n2023\n81\n80\n90\n80\n80\n")\
-        .stdin("3\n1\n5\n")\
-        .stdout("학생 ID: 2023, 평균 성적: 82.00\n", regex=False)\
-        .stdout("원하는 작업을 선택하세요: ", regex=False)\
-        .exit(0)
-
-@check50.check(compiles)
-def test_sum_odd_digits():
-    """학생 ID의 홀수 자릿수 합을 계산하는 기능을 테스트합니다."""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n")\
-        .stdin("1\n2023\n81\n80\n90\n80\n80\n")\
-        .stdin("4\n1\n5\n")\
-        .stdout("학생 ID: 2023, ID의 홀수 자릿수 합: 3\n", regex=False)\
-        .stdout("원하는 작업을 선택하세요: ", regex=False)\
-        .exit(0)
