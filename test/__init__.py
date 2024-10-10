a71a6ccfbd4ae05d62b32f6ef36f52b647484ec3
20201403 <20201403@game.hoseo.edu>
2024-10-10 16:22:43 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -3,34 +3,17 @@ import check50.c
 
 @check50.check()
 def exists():
-    """test.c exists"""
+    """quiz_game.c exists"""
     check50.exists("test.c")
-
+    
 @check50.check(exists)
 def compiles():
-    """test.c compiles"""
-    check50.c.compile("test.c", lcs50=True)
+    """quiz_game.c compiles"""
+    check50.c.compile("test.c", lcs50 = True)
 
 @check50.check(compiles)
-def test1():
+def test_input_1():
     """테스트 케이스1"""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n").stdout("학생이 추가되었습니다.")\
-    .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
-    .stdin("2\n").stdout("짝수 성적의 개수: 8")\
-    .stdin("5\n").stdout("프로그램을 종료합니다. 안녕히 가세요!")
-
-@check50.check(compiles)
-def test2():
-    """테스트 케이스2"""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n").stdout("학생이 추가되었습니다.")\
-    .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
-    .stdin("3\n1\n").stdout("학생 ID: 2023, 평균 성적: 82.20")\
-    .stdin("5\n").stdout("프로그램을 종료합니다. 안녕히 가세요!")
-
-@check50.check(compiles)
-def test3():
-    """테스트 케이스3"""
-    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n").stdout("학생이 추가되었습니다.")\
-    .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
-    .stdin("4\n1\n").stdout("학생 ID: 2023, ID의 홀수 자릿수 합: 3")\
-    .stdin("5\n").stdout("프로그램을 종료합니다. 안녕히 가세요!")
+    check50.run("./test").stdin("1\n").stdout("보통 난이도를 선택하셨습니다. 기본 점수 5점을 획득했습니다.")\
+    .stdin("1\n3\n5\n2\n4\n").stdout("정답 개수: 1\n").stdout("최종 점수: 8")\
+    .stdin("8\n").stdout("좋은 아침입니다! 아침 퀴즈로 하루를 시작하셨네요.").exit(0)
