aa5c49ca80cf25113c0ddb01fcb949a265b7f6a2
20201403 <20201403@game.hoseo.edu>
2024-10-10 14:59:27 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -12,8 +12,8 @@ def compiles():
     check50.c.compile("test.c", lcs50=True)
 
 @check50.check(compiles)
-def test_add_student():
-    """학생 추가 + 짝수 성적 출력 테스트"""
+def test1():
+    """테스트 케이스1"""
     check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n").stdout("학생이 추가되었습니다.")\
     .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
     .stdin("2\n").stdout("짝수 성적의 개수: 8")\
