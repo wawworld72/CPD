68e82f27c09102f13e43175eb9bcefb8a9743ad1
20201403 <20201403@game.hoseo.edu>
2024-10-10 14:33:55 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -13,6 +13,6 @@ def compiles():
 
 @check50.check(compiles)
 def test_add_student():
-    """학생 추가 기능이 정상적으로 작동하는지 테스트합니다."""
+    """학생 추가 테스트"""
     check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1").stdout("학생이 추가되었습니다.\n")
 
