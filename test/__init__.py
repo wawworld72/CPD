989bb80f01a97b93761817d3221988f4a6f7fb27
20201403 <20201403@game.hoseo.edu>
2024-10-10 16:38:02 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -17,3 +17,10 @@ def test_input_1():
     check50.run("./test").stdin("1\n").stdout("보통 난이도를 선택하셨습니다. 기본 점수 5점을 획득했습니다.")\
     .stdin("1\n3\n5\n2\n4\n").stdout("정답 개수: 1\n").stdout("최종 점수: 8")\
     .stdin("8\n").stdout("좋은 아침입니다! 아침 퀴즈로 하루를 시작하셨네요.").exit(0)
+
+@check50.check(compiles)
+def test_input_2():
+    """테스트 케이스2"""
+    check50.run("./test").stdin("2\n").stdout("어려움 난이도를 선택하셨습니다. 추가 점수는 없지만 정답당 점수가 높습니다.")\
+    .stdin("1\n3\n2\n4\n5\n").stdout("정답 개수: 3\n").stdout("최종 점수: 15")\
+    .stdin("20\n").stdout("안녕하세요! 저녁 시간의 퀴즈 도전이군요.").exit(0)
