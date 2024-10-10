0f5ea18ca54e9d2a999c55e1f41aeb880267b9f5
20201403 <20201403@game.hoseo.edu>
2024-10-10 15:18:31 +0900

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -26,3 +26,11 @@ def test2():
     .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
     .stdin("3\n1\n").stdout("학생 ID: 2023, 평균 성적: 82.20")\
     .stdin("5\n").stdout("프로그램을 종료합니다. 안녕히 가세요!")
+
+@check50.check(compiles)
+def test3():
+    """테스트 케이스3"""
+    check50.run("./test").stdin("1\n2024\n51\n60\n50\n60\n50\n1\n").stdout("학생이 추가되었습니다.")\
+    .stdin("1\n2023\n81\n80\n90\n80\n80\n2").stdout("학생이 추가되었습니다.")\
+    .stdin("4\n1\n").stdout("학생 ID: 2023, ID의 홀수 자릿수 합: 3")\
+    .stdin("5\n").stdout("프로그램을 종료합니다. 안녕히 가세요!")
