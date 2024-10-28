4d58bf2efd10d8293d112c7a49dd4b0b2b638a56
20201403 <20201403@game.hoseo.edu>
2024-10-28 21:44:46 +0900

    Update __init__.py

--- substitution_cipher/__init__.py
+++ substitution_cipher/__init__.py
@@ -3,20 +3,18 @@ import check50.c
 
 @check50.check()
 def exists():
-    """time_greeting.c exists"""
-    check50.exists("time_greeting.c")
+    """substitution_cipher.c exists"""
+    check50.exists("substitution_cipher.c")
     
 @check50.check(exists)
 def compiles():
-    """time_greeting.c compiles"""
-    check50.c.compile("time_greeting.c", lcs50 = True)
+    """substitution_cipher.c compiles"""
+    check50.c.compile("substitution_cipher.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_1():
-    """입력 : 13 / 기대 출력 : Good afternoon!"""
-    check50.run("./time_greeting").stdin("13").stdout("Good afternoon!").exit(0)
+    """\n입력 키:** qWeRtYuIoPlKjHgFdSaZxCvBnM\n
+    입력 메시지:** Hello World!\n
+    예상 출력:** 암호문: Itssg Vgksr!\n"""
+    check50.run("./substitution_cipher").stdin("qWeRtYuIoPlKjHgFdSaZxCvBnM").stdin("Hello World!").stdout("Itssg Vgksr!").exit(0)
 
-@check50.check(compiles)
-def test_input_2():
-    """입력 : 20 / 기대 출력 : Good evening!"""
-    check50.run("./time_greeting").stdin("20").stdout("Good evening!").exit(0)
