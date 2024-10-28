8bba59e8ab8b27e9c440d5201da859881701178d
20201403 <20201403@game.hoseo.edu>
2024-10-28 21:52:57 +0900

    Update __init__.py

--- substitution_cipher/__init__.py
+++ substitution_cipher/__init__.py
@@ -9,12 +9,15 @@ def exists():
 @check50.check(exists)
 def compiles():
     """substitution_cipher.c compiles"""
-    check50.c.compile("substitution_cipher.c", lcs50 = True)
+    check50.c.compile("substitution_cipher.c", lcs50=True)
 
 @check50.check(compiles)
 def test_input_1():
     """\n입력 키: qWeRtYuIoPlKjHgFdSaZxCvBnM\n
     입력 메시지: Hello World!\n
     예상 출력: 암호문: Itssg Vgksr!"""
-    check50.run("./substitution_cipher").stdin("qWeRtYuIoPlKjHgFdSaZxCvBnM").stdin("Hello World!").stdout("Itkkg Vgksr!").exit(0)
-
+    check50.run("./substitution_cipher") \
+           .stdin("qWeRtYuIoPlKjHgFdSaZxCvBnM") \
+           .stdin("Hello World!") \
+           .stdout("암호문: Itssg Vgksr!\n") \
+           .exit(0)
