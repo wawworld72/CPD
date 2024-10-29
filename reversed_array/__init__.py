efd7e1b7fe4a01994a5075de27c3d81d5a021610
20201403 <20201403@game.hoseo.edu>
2024-10-29 23:09:58 +0900

    Update __init__.py

--- reversed_array/__init__.py
+++ reversed_array/__init__.py
@@ -13,15 +13,21 @@ def compiles():
 
 @check50.check(compiles)
 def test_case_1():
-    """Test Case 1: Input size 10 with mixed integers"""
-    check50.run("./reversed_array").stdin("1\n2\n4\n5\n7\n8\n9\n0\n3\n6").stdout("6 3 0 9 8 7 5 4 2 1\n").exit(0)
+    """Test Case 1: Input with mixed integers"""
+    check50.run("./reversed_array") \
+        .stdin("1\n2\n4\n5\n7\n8\n9\n0\n3\n6") \
+        .stdout("6 3 0 9 8 7 5 4 2 1\n").exit(0)
 
 @check50.check(compiles)
 def test_case_2():
-    """Test Case 2: Input size 10 with sequential integers"""
-    check50.run("./reversed_array").stdin("1 2 3 4 5 6 7 8 9 0").stdout("0 9 8 7 6 5 4 3 2 1\n").exit(0)
+    """Test Case 2: Input with sequential integers"""
+    check50.run("./reversed_array") \
+        .stdin("1 2 3 4 5 6 7 8 9 0") \
+        .stdout("0 9 8 7 6 5 4 3 2 1\n").exit(0)
 
 @check50.check(compiles)
 def test_case_3():
     """Test Case 3: Input with negative and positive numbers"""
-    check50.run("./reversed_array").stdin("-1 -2 -3 -4 -5 6 7 8 9 10").stdout("10 9 8 7 6 -5 -4 -3 -2 -1\n").exit(0)
+    check50.run("./reversed_array") \
+        .stdin("-1 -2 -3 -4 -5 6 7 8 9 10") \
+        .stdout("10 9 8 7 6 -5 -4 -3 -2 -1\n").exit(0)
