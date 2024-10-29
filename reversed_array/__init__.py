3a43fc9bdc037f031641591b49bb9417d7072d01
20201403 <20201403@game.hoseo.edu>
2024-10-29 23:22:03 +0900

    Update __init__.py

--- reversed_array/__init__.py
+++ reversed_array/__init__.py
@@ -14,14 +14,14 @@ def compiles():
 @check50.check(compiles)
 def test_case_1():
     """Test Case 1: 혼합 정수 입력"""
-    check50.run("./reversed_array").stdin("1\n2\n4\n5\n7\n8\n9\n0\n3\n6\n").stdout("6 3 0 9 8 7 5 4 2 1\n").exit(0)
+    check50.run("./reversed_array").stdin("1\n2\n4\n5\n7\n8\n9\n0\n3\n6").stdout("6 3 0 9 8 7 5 4 2 1").exit(0)
 
 @check50.check(compiles)
 def test_case_2():
     """Test Case 2: 순차적인 정수 입력"""
-    check50.run("./reversed_array").stdin("1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n").stdout("0 9 8 7 6 5 4 3 2 1\n").exit(0)
+    check50.run("./reversed_array").stdin("1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3").stdout("0 9 8 7 6 5 4 3 2 1").exit(0)
 
 @check50.check(compiles)
 def test_case_3():
     """Test Case 3: 음수 및 양수 혼합 입력"""
-    check50.run("./reversed_array").stdin("-1\n-2\n-3\n-4\n-5\n6\n7\n8\n9\n10\n").stdout("10 9 8 7 6 -5 -4 -3 -2 -1\n").exit(0)
+    check50.run("./reversed_array").stdin("-1\n-2\n-3\n-4\n-5\n6\n7\n8\n9\n10").stdout("10 9 8 7 6 -5 -4 -3 -2 -1").exit(0)
