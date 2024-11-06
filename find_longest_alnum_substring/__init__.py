2b24455a5e9470cbae306ea6953a132953c6a0fb
20201403 <20201403@game.hoseo.edu>
2024-11-06 19:43:29 +0900

    Update __init__.py

--- find_longest_alnum_substring/__init__.py
+++ find_longest_alnum_substring/__init__.py
@@ -3,20 +3,15 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """find_longest_alnum_substring.c exists"""
+    check50.exists("find_longest_alnum_substring.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """find_longest_alnum_substring.c compiles"""
+    check50.c.compile("find_longest_alnum_substring.c", lcs50 = True)
 
 @check50.check(compiles)
-def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
-
-@check50.check(compiles)
-def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+def test_input_1():
+    """입력 : i am a boy. / 기대 출력 : boy"""
+    check50.run("./find_longest_alnum_substring").stdin("i am a boy.").stdout("boy").exit(0)
