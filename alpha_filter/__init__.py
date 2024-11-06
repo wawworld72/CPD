9b09f26498225c7e59cdb2d9172ef9124562a6f0
20201403 <20201403@game.hoseo.edu>
2024-11-06 21:01:47 +0900

    Update __init__.py

--- alpha_filter/__init__.py
+++ alpha_filter/__init__.py
@@ -3,20 +3,20 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """alpha_filter.c exists"""
+    check50.exists("alpha_filter.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """alpha_filter.c compiles"""
+    check50.c.compile("alpha_filter.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
+    """입력 : i am a boy / 기대 출력 : iamaboy"""
+    check50.run("./alpha_filter").stdin("i am a boy").stdout("iamaboy").exit(0)
 
 @check50.check(compiles)
 def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+    """입력 : hello   world   w h a t / 기대 출력 : helloworldwhat"""
+    check50.run("./alpha_filter").stdin("hello   world   w h a t").stdout("helloworldwhat").exit(0)
