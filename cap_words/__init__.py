0ae3d370176552dd455717d485fe58c369d8069c
20201403 <20201403@game.hoseo.edu>
2024-11-22 21:21:07 +0900

    Update __init__.py

--- cap_words/__init__.py
+++ cap_words/__init__.py
@@ -9,7 +9,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """cap_words.c compiles"""
-    check50.c.compile("cap_words.c")
+    check50.c.compile("cap_words.c", lcs50 = true)
 
 @check50.check(compiles)
 def test_input_1():
