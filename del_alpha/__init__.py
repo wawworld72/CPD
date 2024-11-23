e2e5d1868623cdfac433019f340a7f2c5c0e6322
20201403 <20201403@game.hoseo.edu>
2024-11-23 19:29:29 +0900

    Update __init__.py

--- del_alpha/__init__.py
+++ del_alpha/__init__.py
@@ -3,20 +3,20 @@ import check50.c
 
 @check50.check()
 def exists():
-    """filter.c exists"""
-    check50.exists("filter.c")
+    """del_alpha.c exists"""
+    check50.exists("del_alpha.c")
     
 @check50.check(exists)
 def compiles():
-    """filter.c compiles"""
-    check50.c.compile("filter.c", lcs50 = True)
+    """del_alpha.c compiles"""
+    check50.c.compile("del_alpha.c", lcs50 = True)
 
 @check50.check(compiles)
 def test_input_15():
-    """입력 : 15 / 기대 출력 : 1 2 4 5 7 8 10"""
-    check50.run("./filter").stdin("15").stdout("1 2 4 5 7 8 10").exit(0)
+    """입력 : Hello, World! 123 / 기대 출력 : HelloWorld"""
+    check50.run("./del_alpha").stdin("Hello, World! 123").stdout("HelloWorld").exit(0)
 
 @check50.check(compiles)
 def test_input_9():
-    """입력 : 9 / 기대 출력 : 1 2 4 5 7 8"""
-    check50.run("./filter").stdin("9").stdout("1 2 4 5 7 8").exit(0)
+    """입력 : ###Hello@@@World### / 기대 출력 : HelloWorld"""
+    check50.run("./del_alpha").stdin("###Hello@@@World###").stdout("HelloWorld").exit(0)
