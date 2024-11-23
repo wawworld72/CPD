048165e899641936a4a144d7a695d000ff5cb93d
20201403 <20201403@game.hoseo.edu>
2024-11-23 20:04:19 +0900

    Update __init__.py

--- del_alpha/__init__.py
+++ del_alpha/__init__.py
@@ -20,3 +20,13 @@ def test_input_15():
 def test_input_9():
     """입력 : ###Hello@@@World### / 기대 출력 : HelloWorld"""
     check50.run("./del_alpha").stdin("###Hello@@@World###").stdout("HelloWorld").exit(0)
+
+@check50.check(compiles)
+def test_input_58():
+    """입력 : The quick brown fox / 기대 출력 : HelloWorld"""
+    check50.run("./del_alpha").stdin("The quick brown fox").stdout("Thequickbrownfox").exit(0)
+
+@check50.check(compiles)
+def test_input_24():
+    """입력 : Word1    Word2     Word3 / 기대 출력 : WordWordWord"""
+    check50.run("./del_alpha").stdin("Word1    Word2     Word3").stdout("WordWordWord").exit(0)
